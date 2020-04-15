bc581589ff59d4a1910651b8cfab89634bab9e2a
Christian Weaver <christian.weaver@mines.sdsmt.edu>
2020-04-14 23:41:29 -0600

    Began Lab 10.

---
+++ Christian_Code/Lab10/Lab10.srcs/sim_1/new/LPU_instructionDecoder_TB.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Author: Christian Weaver
+-- Class: CENG-342
+-- Instructor: Dr. Pyeatt
+-- Date: 04/14/2020
+-- Lab 10
+-- Design Name: LPU_instructionDecoder
+-- Project Name: Lab10
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use work.instructionDecoderPKG.all;
+
+entity LPU_instructionDecoder_TB is
+end LPU_instructionDecoder_TB;
+
+architecture tb_arch of LPU_instructionDecoder_TB is
+    signal I : std_logic_vector(15 downto 0); -- instruction to decode
+    signal take_branch: std_logic_vector(3 downto 0); -- input from BTU
+    signal T : instruction_t; -- instruction type
+    signal imm: std_logic_vector(31 downto 0); -- immediate data field
+    signal Asel: std_logic_vector(2 downto 0); -- select for register A
+    signal Bsel: std_logic_vector(2 downto 0); -- select for register B
+    signal Dsel: std_logic_vector(2 downto 0); -- select for register D
+    signal ALUfunc: std_logic_vector(3 downto 0); -- function for ALU
+    signal control: control_t_array; -- Mux and register enable signals
+begin
+    decoder:
+        entity work.LPU_instructionDecoder(arch)
+        port map(
+            I => I,
+            take_branch => take_branch,
+            T => T,
+            imm => imm,
+            Asel => Asel,
+            Bsel => Bsel,
+            Dsel => Dsel,
+            ALUfunc => ALUfunc,
+            control => control          
+            );
+            
+            
+    process
+    begin
+        for j in 0 to 2**20-1 loop
+            I <= std_logic_vector(to_unsigned(j, 20)(19 downto 4));
+            take_branch <= std_logic_vector(to_unsigned(j, 20)(3 downto 0));
+            
+            wait for 20 ns;
+        end loop;
+    
+    end process;
+
+end tb_arch;
--- Christian_Code/Lab10/Lab10.srcs/sources_1/new/LPU_instructionDecoder.vhd
+++ Christian_Code/Lab10/Lab10.srcs/sources_1/new/LPU_instructionDecoder.vhd
@@ -2,7 +2,7 @@
 -- Author: Christian Weaver
 -- Class: CENG-342
 -- Instructor: Dr. Pyeatt
--- Date: 04/11/2020
+-- Date: 04/14/2020
 -- Lab 10
 -- Design Name: LPU_instructionDecoder
 -- Project Name: Lab10
@@ -14,10 +14,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.instructionDecoderPKG.all;
 
-entity instructionDecoder is
+entity LPU_instructionDecoder is
     port(
         I : in std_logic_vector(15 downto 0); -- instruction to decode
-        take_branch: in std_vector; -- input from BTU
+        take_branch: in std_logic_vector(3 downto 0); -- input from BTU
         T : out instruction_t; -- instruction type
         imm: out std_logic_vector(31 downto 0); -- immediate data field
         Asel: out std_logic_vector(2 downto 0); -- select for register A
@@ -26,11 +26,55 @@ entity instructionDecoder is
         ALUfunc: out std_logic_vector(3 downto 0); -- function for ALU
         control: out control_t_array -- Mux and register enable signals
         );
-end instructionDecoder;
+end LPU_instructionDecoder;
 
 
-architecture arch of instructionDecoder is
+architecture arch of LPU_instructionDecoder is
 begin
+
+    Asel <= I(2 downto 0) when I(15 downto 6) = "1000011000" else -- CMPR
+        I(2 downto 0) when I(15 downto 13) = "100" and I(10 downto 8) = "111" else -- CMPI
+        I(2 downto 0) when I(15 downto 13) = "100" and I(10 downto 9) = "10" and I(7 downto 6) = "00" else -- RR
+        I(5 downto 3) when I(15 downto 13) = "100" and I(10) = '0' else -- RRR
+        I(2 downto 0) when I(15 downto 13) = "110" else -- RI
+        I(5 downto 3) when I(15 downto 13) = "101" else -- RRI
+        "XXX" when I(15 downto 14) = "00" and I(5 downto 3) = "111" else -- PCRL
+        I(5 downto 3) when I(15 downto 14) = "00" and I(5 downto 3) /= "111" else -- LOAD (this isn't exclusive from PCRL
+        I(5 downto 3) when I(15 downto 14) = "01" else -- STORE
+        I(2 downto 0) when I(15 downto 12) = "1110" and I(6 downto 3) = "0000" else -- BR
+        "XXX" when I(15 downto 12) = "1111" else -- BPCR
+        "XXX" when I(15 downto 12) = "1110" and I(6 downto 3) = "1111" else -- HCF
+        "XXX"; -- ILLEGAL
+        
+
+    Bsel <= I(5 downto 3) when I(15 downto 6) = "1000011000" else -- CMPR
+        "XXX" when I(15 downto 13) = "100" and I(10 downto 8) = "111" else -- CMPI
+        I(5 downto 3) when I(15 downto 13) = "100" and I(10 downto 9) = "10" and I(7 downto 6) = "00" else -- RR
+        I(8 downto 6) when I(15 downto 13) = "100" and I(10) = '0' else -- RRR
+        "XXX" when I(15 downto 13) = "110" else -- RI
+        "XXX" when I(15 downto 13) = "101" else -- RRI
+        "XXX" when I(15 downto 14) = "00" and I(5 downto 3) = "111" else -- PCRL
+        "XXX" when I(15 downto 14) = "00" and I(5 downto 3) /= "111" else -- LOAD (this isn't exclusive from PCRL
+        I(2 downto 0) when I(15 downto 14) = "01" else -- STORE
+        "XXX" when I(15 downto 12) = "1110" and I(6 downto 3) = "0000" else -- BR
+        "XXX" when I(15 downto 12) = "1111" else -- BPCR
+        "XXX" when I(15 downto 12) = "1110" and I(6 downto 3) = "1111" else -- HCF
+        "XXX"; -- ILLEGAL
+        
+    Dsel <= "XXX" when I(15 downto 6) = "1000011000" else -- CMPR
+        "XXX" when I(15 downto 13) = "100" and I(10 downto 8) = "111" else -- CMPI
+        I(2 downto 0) when I(15 downto 13) = "100" and I(10 downto 9) = "10" and I(7 downto 6) = "00" else -- RR
+        I(2 downto 0) when I(15 downto 13) = "100" and I(10) = '0' else -- RRR
+        I(2 downto 0) when I(15 downto 13) = "110" else -- RI
+        I(2 downto 0) when I(15 downto 13) = "101" else -- RRI
+        I(2 downto 0) when I(15 downto 14) = "00" and I(5 downto 3) = "111" else -- PCRL
+        I(2 downto 0) when I(15 downto 14) = "00" and I(5 downto 3) /= "111" else -- LOAD (this isn't exclusive from PCRL
+        "XXX" when I(15 downto 14) = "01" else -- STORE
+        "111" when I(15 downto 12) = "1110" and I(6 downto 3) = "0000" else -- BR
+        "111" when I(15 downto 12) = "1111" else -- BPCR
+        "XXX" when I(15 downto 12) = "1110" and I(6 downto 3) = "1111" else -- HCF
+        "XXX"; -- ILLEGAL
+    
 
 
 end arch;
